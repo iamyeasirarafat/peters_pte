225369e81d605b0743cfba9a1acb16bf55d32f50
eshakkhan29 <eshakkhan29@gmail.com>
2023-08-11 06:54:16 +0000

    summarize_written fix

--- app/app/practice/summarize_written/page.js
+++ app/app/practice/summarize_written/page.js
@@ -62,40 +62,40 @@ const Index = () => {
         <TextBlock />
         {/* type Block */}
         <TypingBlock />
-        <div className="relative border border-primary rounded-[15px] mt-12 py-4 px-8">
-          {/* tab button */}
-          <div className="flex items-center gap-x-2 absolute bottom-[101%] right-5">
-            <TabButton
-              src={"/icons/aplus.svg"}
-              iconWidth={21}
-              iconHeight={23}
-              bgColor={"blue"}
-              textColor={"white"}
-            >
-              My Score
-            </TabButton>
-            <TabButton
-              src={"/icons/massage.svg"}
-              iconWidth={22}
-              iconHeight={22}
-              bgColor={"cream"}
-              textColor={"gray"}
-            >
-              Forum
-            </TabButton>
-            <TabButton
-              src={"/icons/score.svg"}
-              iconWidth={24}
-              iconHeight={24}
-              bgColor={"primary"}
-              textColor={"gray"}
-            >
-              Community Score
-            </TabButton>
-          </div>
-          {/* score */}
-          <Score />
+      </div>
+      <div className="relative border border-primary rounded-[15px] mt-12 py-4 px-8">
+        {/* tab button */}
+        <div className="flex items-center gap-x-2 absolute bottom-[101%] right-5">
+          <TabButton
+            src={"/icons/aplus.svg"}
+            iconWidth={21}
+            iconHeight={23}
+            bgColor={"blue"}
+            textColor={"white"}
+          >
+            My Score
+          </TabButton>
+          <TabButton
+            src={"/icons/massage.svg"}
+            iconWidth={22}
+            iconHeight={22}
+            bgColor={"cream"}
+            textColor={"gray"}
+          >
+            Forum
+          </TabButton>
+          <TabButton
+            src={"/icons/score.svg"}
+            iconWidth={24}
+            iconHeight={24}
+            bgColor={"primary"}
+            textColor={"gray"}
+          >
+            Community Score
+          </TabButton>
         </div>
+        {/* score */}
+        <Score />
       </div>
     </div>
   );
