caac17208cb331022c7f610217f27aba999edc5f
eshakkhan29 <eshakkhan29@gmail.com>
2023-09-17 01:43:40 +0600

    Multiple Choice, Single Answers

--- src/components/global/MultipleChoiceAnswer.jsx
+++ src/components/global/MultipleChoiceAnswer.jsx
@@ -1,33 +1,6 @@
 import React from "react";
 
-const answers = [
-  {
-    serial: "A",
-    answer:
-      "Listen to the recording and answer the question by selecting all the tortoise",
-  },
-  {
-    serial: "B",
-    answer:
-      "Listen to the recording and answer the question by selecting all the tortoise and Listen to the recording and answer the question by selecting all the tortoise Listen to the recording and answer the question by selecting all the tortoise and Listen to the recording and answer the question by selecting all the tortoise",
-  },
-  {
-    serial: "C",
-    answer:
-      "Listen to the recording and answer the question by selecting all the question color fact on global earth",
-  },
-  {
-    serial: "D",
-    answer:
-      "Listen to the recording and answer the question by selecting all the tortoise and Listen to the recording and answer the question by selecting all the tortoise",
-  },
-  {
-    serial: "E",
-    answer:
-      "Listen to the recording and answer the question by selecting all the question color fact on global earth",
-  },
-];
-function MultipleChoiceAnswer() {
+function MultipleChoiceAnswer({ register, answers }) {
   return (
     <div className="p-5 rounded-[15px] border border-primary">
       <p className="text-lg text-center">
@@ -36,7 +9,7 @@ function MultipleChoiceAnswer() {
       </p>
       <div className="space-y-2 mt-2">
         {answers.map((answer, i) => (
-          <Answer key={i} answer={answer} />
+          <Answer key={i} answer={answer} register={register} />
         ))}
       </div>
     </div>
@@ -45,10 +18,11 @@ function MultipleChoiceAnswer() {
 
 export default MultipleChoiceAnswer;
 
-const Answer = ({ answer }) => {
+export const Answer = ({ answer, register }) => {
   return (
     <label className="bg-secondary rounded-[15px] border border-primary p-3 flex items-center gap-x-3 cursor-pointer">
       <input
+        {...register(`${answer?.serial}`)}
         className="border-2 border-primary focus:ring-transparent cursor-pointer w-7 h-7 rounded-md text-primary"
         type="checkbox"
         name=""
---
+++ src/components/global/SingleChoiceAnswer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+
+function SingleChoiceAnswer({ register, answers }) {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  return (
+    <div className="p-5 rounded-[15px] border border-primary">
+      <p className="text-lg text-center">
+        Based on the audio, which of the following is a true statement about
+        short story writing?
+      </p>
+      <div className="space-y-2 mt-2">
+        {answers.map((answer, i) => (
+          <label
+            key={i}
+            className="bg-secondary rounded-[15px] border border-primary p-3 flex items-center gap-x-3 cursor-pointer"
+          >
+            <input
+              {...register(`${answer?.serial}`)}
+              className="border-2 border-primary focus:ring-transparent cursor-pointer w-7 h-7 rounded-md text-primary"
+              type="checkbox"
+              checked={selectedAnswer === answer?.serial}
+              onChange={() => setSelectedAnswer(`${answer?.serial}`)}
+            />
+            <p className="text-gray flex items-center gap-x-5 text-xl">
+              <span className="capitalize">{answer?.serial}.</span>
+              <span className="text-base">{answer?.answer}</span>
+            </p>
+          </label>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default SingleChoiceAnswer;
