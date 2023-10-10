b201669071e8764db0f9122ac3847217765eb7f5
iamyeasirarafat <yas.yeasir@gmail.com>
2023-10-11 02:07:01 +0600

    multiple choice single and select missing word api added

--- src/components/global/SingleChoiceAnswer.jsx
+++ src/components/global/SingleChoiceAnswer.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
-
-function SingleChoiceAnswer({ register, answers }) {
-  const [selectedAnswer, setSelectedAnswer] = useState("");
-
+function SingleChoiceAnswer({
+  register,
+  answers,
+  selectedAnswer,
+  setSelectedAnswer,
+  setGivenAnswer,
+}) {
   return (
     <div className="p-5 rounded-[15px] border border-primary">
       <p className="text-lg text-center">
@@ -10,13 +12,15 @@ function SingleChoiceAnswer({ register, answers }) {
         short story writing?
       </p>
       <div className="space-y-2 mt-2">
-        {answers.map((answer, i) => (
+        {answers?.map((answer, i) => (
           <Answer
             key={i}
             answer={answer}
+            serial={i}
             register={register}
             selectedAnswer={selectedAnswer}
             setSelectedAnswer={setSelectedAnswer}
+            setGivenAnswer={setGivenAnswer}
           />
         ))}
       </div>
@@ -26,25 +30,42 @@ function SingleChoiceAnswer({ register, answers }) {
 
 export default SingleChoiceAnswer;
 
-const Answer = ({ register, answer, setSelectedAnswer, selectedAnswer }) => {
+export const Answer = ({
+  answer,
+  register,
+  serial,
+  setSelectedAnswer,
+  selectedAnswer,
+  setGivenAnswer,
+}) => {
+  const obj = {
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+  };
   return (
     <label
       className={`${
-        selectedAnswer === answer?.serial ? "bg-secondary" : "bg-white"
+        selectedAnswer === answer ? "bg-secondary" : "bg-white"
       } rounded-[15px] border border-primary p-3 flex items-center gap-x-3 cursor-pointer`}
     >
       <input
-        {...register(`${answer?.serial}`)}
+        {...register(`${serial}`)}
         className="border-2 border-primary focus:ring-transparent cursor-pointer w-7 h-7 rounded-md text-primary"
         type="checkbox"
-        checked={selectedAnswer === answer?.serial}
-        onChange={() => {
-          setSelectedAnswer(`${answer?.serial}`);
+        checked={selectedAnswer === answer}
+        onClick={() => {
+          setSelectedAnswer(answer);
+          setGivenAnswer([serial + 1]);
         }}
       />
       <p className="text-gray flex items-center gap-x-5 text-xl">
-        <span className="capitalize">{answer?.serial}.</span>
-        <span className="text-base">{answer?.answer}</span>
+        <span className="capitalize">{obj[serial + 1]}.</span>
+        <span className="text-base">{answer}</span>
       </p>
     </label>
   );
