bcd7ae21b9ceb376909c6de5bc4e30434f77bd4e
eshakkhan29 <eshakkhan29@gmail.com>
2023-10-22 00:46:35 +0600

    reports and calender page

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -7,65 +7,61 @@ import Item from "./Item";
 
 import { useHydrated } from "@/hooks/useHydrated";
 
-type ProductsProps = {
-    items: any;
+type StudentsProps = {
+  items: any;
 };
 
-const Products = ({ items }: ProductsProps) => {
-    const [valueAll, setValueAll] = useState<boolean>(false);
-    const { mounted } = useHydrated();
+const Students = ({ items }: StudentsProps) => {
+  const [valueAll, setValueAll] = useState<boolean>(false);
+  const { mounted } = useHydrated();
 
-    const isTablet = useMediaQuery({
-        query: "(max-width: 1023px)",
-    });
+  const isTablet = useMediaQuery({
+    query: "(max-width: 1023px)",
+  });
 
-    return mounted && isTablet ? (
-        <div className="card">
-            {items.map((product: any) => (
-                <Item item={product} key={product.id} />
-            ))}
-        </div>
-    ) : (
-        <table className="table-custom table-select">
-            <thead>
-                <tr>
-                    <th className="th-custom">
-                        <Checkbox
-                            value={valueAll}
-                            onChange={() => setValueAll(!valueAll)}
-                        />
-                    </th>
-                    <th className="th-custom">
-                        <Sorting title="Product" />
-                    </th>
-                    <th className="th-custom">
-                        <Sorting title="Category" />
-                    </th>
-                    <th className="th-custom">
-                        <Sorting title="Id" />
-                    </th>
-                    <th className="th-custom">
-                        <Sorting title="Avl" />
-                    </th>
-                    <th className="th-custom">
-                        <Sorting title="Color" />
-                    </th>
-                    <th className="th-custom text-right">
-                        <Sorting title="Sales" />
-                    </th>
-                    <th className="th-custom text-right">
-                        <Sorting title="Price" />
-                    </th>
-                    <th className="th-custom text-right"></th>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map((product: any) => (
-                    <Row item={product} key={product.id} />
-                ))}
-            </tbody>
-        </table>
-    );
+  return mounted && isTablet ? (
+    <div className="bg-white dark:bg-black">
+      {items.map((product: any, i: number) => (
+        <Item item={product} key={i} />
+      ))}
+    </div>
+  ) : (
+    <table className="bg-white dark:bg-black">
+      <thead>
+        <tr>
+          <th className="th-custom">
+            <Checkbox
+              value={valueAll}
+              onChange={() => setValueAll(!valueAll)}
+            />
+          </th>
+          <th className="th-custom">
+            <Sorting title="Student Name" />
+          </th>
+          <th className="th-custom">
+            <Sorting title="Account Plan" />
+          </th>
+          <th className="th-custom">
+            <Sorting title="User Id" />
+          </th>
+          <th className="th-custom">
+            <Sorting title="Last Logged In" />
+          </th>
+          <th className="th-custom">
+            <Sorting title="Average Score" />
+          </th>
+          <th className="th-custom text-right">
+            <Sorting title="Group" />
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((product: any, i: number) => (
+          <Row item={product} key={i} />
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
-export default Products;
+export default Students;
