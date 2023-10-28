d4f4cd833406c0aa031a19b4d92d2034391bc2cd
developertanvir2019 <developertanvir2019@gmail.com>
2023-10-28 15:22:25 +0600

    form creted for read aloud

---
+++ pages/admin/practice-question/[questionTable]/create.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AdminLayout from "@/components/AdminLayout";
+import { useRouter } from "next/router";
+import ReadAloud from "@/components/QuestionForms/ReadAloud";
+import RepeatSentence from "@/components/QuestionForms/RepeatSentence";
+const AllForms = () => {
+  const router = useRouter();
+  const { questionTable } = router.query;
+
+  let content;
+  if (questionTable == "Read Aloud") {
+    content = <ReadAloud />;
+  } else if (questionTable == "Repeat Sentence") {
+    content = <RepeatSentence />;
+  } else {
+    content = <h2>this is new question from</h2>;
+  }
+  return (
+    <AdminLayout title={`${questionTable} / New Question`}>
+      {content}
+    </AdminLayout>
+  );
+};
+
+export default AllForms;
--- pages/admin/practice-question/[questionTable]/index.jsx
+++ pages/admin/practice-question/[questionTable]/index.jsx
@@ -2,7 +2,9 @@ import { useRouter } from "next/router";
 import AdminLayout from "@/components/AdminLayout";
 import Table from "@/components/QuestionTable";
 import TablePagination from "@/components/TablePagination";
-const QuestionTable = () => {
+import Icon from "@/components/Icon";
+import Link from "next/link";
+const Index = () => {
   const studentsList = [
     {
       name: "Bill on the hill",
@@ -51,12 +53,33 @@ const QuestionTable = () => {
   const { questionTable } = router.query;
   return (
     <AdminLayout title={questionTable}>
-      <div>
-        <Table student={false} items={studentsList} />
-        <TablePagination />
+      <div className="flex justify-between mb-6">
+        <div>
+          <Link href={`/admin/practice-question/${questionTable}/create`}>
+            <button className="mr-3 h-10 px-6 text-sm font-bold last:mb-0 bg-orange-300 transition-colors hover:bg-n-3/10 dark:hover:bg-white/20">
+              <Icon
+                className="-mt-0.25 mr-3 dark:fill-white"
+                name="add-circle"
+              />
+              Create New Question
+            </button>
+          </Link>
+          <button
+            className="h-10 px-6 text-sm font-bold last:mb-0 bg-white  transition-colors hover:bg-n-3/10 dark:hover:bg-white/20"
+            as="button"
+          >
+            <Icon className="-mt-0.25 mr-3 dark:fill-white" name="filters" />
+            filters
+          </button>
+        </div>
+        <button className="px-3 py-[0.4rem] border bg-white">
+          <Icon className=" dark:fill-white" name="search" />
+        </button>
       </div>
+      <Table student={false} items={studentsList} />
+      <TablePagination />
     </AdminLayout>
   );
 };
 
-export default QuestionTable;
+export default Index;
