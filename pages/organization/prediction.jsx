6271a34b1705e3b9d1c7dd53d9922303858e2f19
eshakkhan29 <eshakkhan29@gmail.com>
2023-12-23 09:55:11 +0600

    study materials

--- pages/organization/prediction.jsx
+++ pages/organization/prediction.jsx
@@ -2,26 +2,48 @@ import Layout from "@/components/Layout";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { StudyFile } from "./study-material";
+import TablePagination from "@/components/TablePagination";
+// import AnimatedLoading from "@"
 function Prediction() {
+  const [loading, setLoading] = useState(true);
   const [prediction, setPrediction] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const pageLimit = 9;
 
   // get Data
   useEffect(() => {
     const getStudyPrediction = async () => {
-      const res = await axios.get(`/study_materials/prediction`);
+      const res = await axios.get(
+        `/study_materials/prediction?limit=${pageLimit}&page=${pageNumber}`
+      );
       setPrediction(res?.data);
+      setLoading(false);
     };
     getStudyPrediction();
-  }, []);
+  }, [pageNumber]);
   return (
     <Layout title="Prediction">
       <p className="text-lg font-extrabold mb-2">Prediction File</p>
       {/* file */}
-      <div className={`${prediction?.length > 0 ? "space-y-2" : ""}`}>
-        {prediction?.results?.map((item, i) => (
-          <StudyFile key={item.id} data={item} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-96">
+          <div
+            className="w-12 h-12 rounded-full animate-spin
+                  border-x-8 border-solid border-orange-400 border-t-transparent"
+          ></div>
+        </div>
+      ) : (
+        <div className={`${prediction?.length > 0 ? "space-y-2" : ""}`}>
+          {prediction?.results?.map((item, i) => (
+            <StudyFile key={item.id} data={item} />
+          ))}
+          <TablePagination
+            pageNumber={pageNumber}
+            totalPage={Math.ceil(prediction?.total / pageLimit)}
+            prevNext={setPageNumber}
+          />
+        </div>
+      )}
     </Layout>
   );
 }
--- pages/organization/study-material.jsx
+++ pages/organization/study-material.jsx
@@ -3,23 +3,31 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BiSolidDownload } from "react-icons/bi";
 import { getFileName } from "../../utils/getFileName";
+import { formatDateTime } from "@/utils/formatDateTime";
+import TablePagination from "@/components/TablePagination";
 const FilterTab = [
   { name: "Grammar" },
   { name: "Vocabulary" },
   { name: "Essay" },
 ];
 function StudyMaterial() {
+  const [loading, setLoading] = useState(true);
   const [activeTab, setActiveTab] = useState("Grammar");
   const [studyMaterial, setStudyMaterial] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const pageLimit = 9;
 
   // get Data
   useEffect(() => {
     const getStudyMaterial = async () => {
-      const res = await axios.get(`/study_materials/study_material`);
+      const res = await axios.get(
+        `/study_materials/study_material?limit=${pageLimit}&page=${pageNumber}`
+      );
       setStudyMaterial(res?.data);
+      setLoading(false);
     };
     getStudyMaterial();
-  }, []);
+  }, [pageNumber]);
   return (
     <Layout title="Study Material">
       <p className="text-lg font-extrabold mb-2">Topic</p>
@@ -40,11 +48,25 @@ function StudyMaterial() {
         ))}
       </div>
       {/* file */}
-      <div className={`${studyMaterial?.length > 0 ? "space-y-2" : ""}`}>
-        {studyMaterial?.results?.map((material, i) => (
-          <StudyFile key={material?.id} data={material} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-96">
+          <div
+            className="w-12 h-12 rounded-full animate-spin
+                  border-x-8 border-solid border-orange-400 border-t-transparent"
+          ></div>
+        </div>
+      ) : (
+        <div className={`${studyMaterial?.length > 0 ? "space-y-2" : ""}`}>
+          {studyMaterial?.results?.map((material, i) => (
+            <StudyFile key={material?.id} data={material} />
+          ))}
+          <TablePagination
+            pageNumber={pageNumber}
+            totalPage={Math.ceil(studyMaterial?.total / pageLimit)}
+            prevNext={setPageNumber}
+          />
+        </div>
+      )}
     </Layout>
   );
 }
@@ -52,7 +74,9 @@ function StudyMaterial() {
 export default StudyMaterial;
 
 export const handlePdfDownload = (name) => {
-  const blob = new Blob([name?.file], { type: "application/pdf" });
+  const blob = new Blob([`https://api.codebyamirus.link/${name?.file}`], {
+    type: "application/pdf",
+  });
   const url = window.URL.createObjectURL(blob);
   const link = document.createElement("a");
   link.href = url;
@@ -66,7 +90,9 @@ export const handlePdfDownload = (name) => {
 export const StudyFile = ({ data }) => {
   return (
     <div className="px-5 py-6 bg-white dark:bg-black flex items-center justify-between">
-      <p className="text-sm font-bold">{data?.title}</p>
+      <p className="text-sm font-bold">
+        {data?.title} {formatDateTime(data?.uploaded_at, "date")}
+      </p>
       <button onClick={() => handlePdfDownload(data)}>
         <BiSolidDownload className="text-xl" />
       </button>
