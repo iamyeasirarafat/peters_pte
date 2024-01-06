60d7548777ef0c9625fc5c2c028caea4bbd5be9a
mdimranh <mdimranh750@gmail.com>
2024-01-07 00:34:11 +0800

    deleted default pagelist and registration page

--- pages/registration.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import RegistrationPage from "@/templates/RegistrationPage";
-
-const Registration: NextPage = () => {
-    return <RegistrationPage />;
-};
-
-export default Registration;
