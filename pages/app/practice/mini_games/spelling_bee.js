acc028b585f2a3d3bc11603a8d78fe2336ac35b7
eshakkhan29 <eshakkhan29@gmail.com>
2024-03-30 23:59:21 +0600

    fix audio

---
+++ pages/app/practice/mini_games/spelling_bee.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DashboardLayout from "../../layout";
+// import DashboardLayout from "./layout";
+
+function SpellingBee() {
+  return (
+    <DashboardLayout dashboard>
+      <h1>SpellingBee</h1>
+    </DashboardLayout>
+  );
+}
+
+export default SpellingBee;
