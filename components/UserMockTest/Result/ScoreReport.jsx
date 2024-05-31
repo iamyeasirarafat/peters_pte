478b8ba016316bf790380f066d5e5b93e7372c66
eshakkhan29 <eshakkhan29@gmail.com>
2024-06-01 01:00:48 +0600

    mocktest result added

---
+++ components/UserMockTest/Result/ScoreReport.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Score from "./Score";
+import ScoreTable from "./ScoreTable";
+
+const scoreReportTab = [
+  "score report",
+  "speaking",
+  "reading",
+  "writing",
+  "listening",
+];
+const ScoreReport = () => {
+  const [activeTab, setActiveTab] = useState("score report");
+  return (
+    <div className="bg-white border border-primary rounded-[15px]">
+      {/* tab */}
+      <div className="flex gap-12 px-3">
+        {scoreReportTab.map((item, i) => (
+          <button
+            onClick={() => setActiveTab(item)}
+            key={i}
+            className={`${
+              activeTab === item
+                ? "text-primary  border-primary"
+                : "text-gray border-transparent"
+            } py-5 px-3 text-lg capitalize  border-b-2`}
+          >
+            {item}
+          </button>
+        ))}
+      </div>
+      <hr className="border border-secondary" />
+      {/* result */}
+      <div className="px-6">
+        {activeTab === "score report" ? (
+          <Score setActiveTab={setActiveTab} />
+        ) : (
+          <ScoreTable />
+        )}
+      </div>
+    </div>
+  );
+};
+export default ScoreReport;
