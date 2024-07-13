31a42201a47031b222a220faf01bbae71ce174cf
iamyeasirarafat <yas.yeasir@gmail.com>
2024-07-14 00:24:22 +0600

    promo banner added and add mocktest question added

--- pages/admin/mocktest/add-mocktest/reading_mocktests.jsx
+++ pages/admin/mocktest/add-mocktest/reading_mocktests.jsx
@@ -1,13 +1,12 @@
-import Layout from "@/components/Layout";
 import Field from "@/components/Field";
-import { useForm } from "react-hook-form";
-import { getQuestion, optionFormatter } from "./full_mocktests";
-import { useState } from "react";
-import { useEffect } from "react";
+import Layout from "@/components/Layout";
 import axios from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import Spinner from "../../../../components/Spinner/Spinner";
-import { useRouter } from "next/router";
+import { getQuestion, optionFormatter } from "./full_mocktests";
 import MockTestMultiSelector from "./MockTestMultiSelector";
 function FullMocktest() {
   const router = useRouter();
@@ -24,17 +23,25 @@ export default FullMocktest;
 const ListeningTestForm = ({ id }) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+  const [readAloud, setReadAloud] = useState([]);
   const [rwblanks, setRwblanks] = useState([]);
-  const [listeningMcqs, setListeningMcqs] = useState([]);
+  const [readingMcqs, setReadingMcqs] = useState([]);
   const [reorderParagraphs, setReorderParagraphs] = useState([]);
-  const [listeningBlancks, setListeningBlancks] = useState([]);
-  const [listeningMcqSingles, setListeningMcqSingles] = useState([]);
+  const [readingBlancks, setReadingBlancks] = useState([]);
+  const [readingMcqSingles, setReadingMcqSingles] = useState([]);
+  const [highlightIncorrect, setHighlightIncorrect] = useState([]);
+  const [summarizes, setSummarizes] = useState([]);
+  const [highlightSummarys, setHighlightSummarys] = useState([]);
   useEffect(() => {
+    getQuestion("/practice/read_alouds", setReadAloud);
     getQuestion("/read-write/blanks", setRwblanks);
-    getQuestion("/multi_choices", setListeningMcqs);
+    getQuestion("/multi_choices/reading", setReadingMcqs);
     getQuestion("/reorder_paragraphs", setReorderParagraphs);
-    getQuestion("/blanks", setListeningBlancks);
-    getQuestion("/multi_choices/single-answer", setListeningMcqSingles);
+    getQuestion("/reading_blanks", setReadingBlancks);
+    getQuestion("/multi_choices/reading/single-answer", setReadingMcqSingles);
+    getQuestion("/highlight_incorrect_words", setHighlightIncorrect);
+    getQuestion("/summarizes", setSummarizes);
+    getQuestion("/highlight_summarys", setHighlightSummarys);
   }, []);
   const {
     register,
@@ -45,18 +52,10 @@ const ListeningTestForm = ({ id }) => {
   } = useForm();
   const onSubmit = async (data) => {
     setIsLoading(true);
-    const formData = {
-      title: data?.title,
-      reading_writting_blank: data?.reading_writting_blank || [],
-      multi_choice_multi_answer: data?.multi_choice_multi_answer || [],
-      multi_choice_single_answer: data?.multi_choice_single_answer || [],
-      reorder_paragraph: data?.reorder_paragraph || [],
-      blank: data?.blank || [],
-    };
     try {
       const res = id
-        ? await axios.put(`reading_mocktest/${id}`, formData)
-        : await axios.post("/reading_mocktest", formData);
+        ? await axios.put(`reading_mocktest/${id}`, data)
+        : await axios.post("/reading_mocktest", data);
       toast.success(`Mocktest ${id ? "update" : "added"} successfully`);
       reset();
       setIsLoading(false);
@@ -67,9 +66,11 @@ const ListeningTestForm = ({ id }) => {
         toast.error(error?.response?.data?.title[0] || "Something went wrong");
     }
   };
-  const listeningMcqsOptions = optionFormatter(listeningMcqs);
+  const readingMcqsOptions = optionFormatter(readingMcqs);
   const reorderParagraphsOptions = optionFormatter(reorderParagraphs);
-  const listeningMcqSinglesOptions = optionFormatter(listeningMcqSingles);
+  const readingMcqSinglesOptions = optionFormatter(readingMcqSingles);
+  const readWriteBlancksOptions = optionFormatter(rwblanks);
+  const highlightSummarysOptions = optionFormatter(highlightSummarys);
   // set Default data
   useEffect(() => {
     if (id) {
@@ -94,20 +95,34 @@ const ListeningTestForm = ({ id }) => {
         register={register}
         name="title"
       />
-      <div className="grid grid-cols-3 gap-x-5">
+      <div className="grid grid-cols-3 gap-5">
         <MockTestMultiSelector
-          options={rwblanks}
+          options={readAloud}
+          control={control}
+          name="read_aloud"
+          placeholder="Select Read Aloud"
+          label="Read Aloud"
+        />
+        <MockTestMultiSelector
+          options={readWriteBlancksOptions}
           control={control}
           name="reading_writting_blank"
           placeholder="Select Reading & Writing: FIB"
           label="Reading & Writing: FIB"
         />
         <MockTestMultiSelector
-          options={listeningMcqsOptions}
+          options={readingMcqsOptions}
           control={control}
-          name="multi_choice_multi_answer"
+          name="multi_choice_reading_multi_answer"
           placeholder="Select Multiple Choice (Multiple)"
           label="Multiple Choice (Multiple)"
+        />
+        <MockTestMultiSelector
+          options={readingMcqSinglesOptions}
+          control={control}
+          name="multi_choice_reading_single_answer"
+          placeholder="Select Multiple Choice (Single)"
+          label="Multiple Choice (Single)"
         />
         <MockTestMultiSelector
           options={reorderParagraphsOptions}
@@ -116,21 +131,33 @@ const ListeningTestForm = ({ id }) => {
           placeholder="Select Re-order Paragraphs"
           label="Re-order Paragraphs"
         />
-      </div>
-      <div className="grid grid-cols-3 gap-x-5">
         <MockTestMultiSelector
-          options={listeningBlancks}
+          options={readingBlancks}
           control={control}
-          name="blank"
+          name="reading_blank"
           placeholder="Select Reading: Fill in the Blanks"
           label="Reading: Fill in the Blanks"
         />
         <MockTestMultiSelector
-          options={listeningMcqSinglesOptions}
+          options={highlightIncorrect}
           control={control}
-          name="multi_choice_single_answer"
-          placeholder="Select Multiple Choice (Single)"
-          label="Multiple Choice (Single)"
+          name="highlight_incorrect_word"
+          placeholder="Select Highlight Incorrect Words"
+          label="Highlight Incorrect Words"
+        />
+        <MockTestMultiSelector
+          options={summarizes}
+          control={control}
+          name="summarize"
+          placeholder="Select Summarize Written Text"
+          label="Summarize Written Text"
+        />
+        <MockTestMultiSelector
+          options={highlightSummarysOptions}
+          control={control}
+          name="highlight_summary"
+          placeholder="Select Highlight Correct Summary"
+          label="Highlight Correct Summary"
         />
       </div>
       <button
